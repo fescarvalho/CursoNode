7e769cfb6b9b881ed866bd1f3f326f7e28f5ba5e
fescarvalho <fernando_scarvalho@hotmail.com>
2022-10-27 21:04:26 -0300

    create model Useer and connection in database

--- apinodeRelacional/src/routes.js
+++ apinodeRelacional/src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/teste', (req, res) => {
-  res.json({ message: 'ok' });
+routes.get('/teste', async (req, res) => {
+  const user = await User.create({
+    name: 'Fernando',
+    email: 'fernando@fernando.com',
+    password_hash: '123456',
+  });
+  res.json(user);
 });
 
 export default routes;
